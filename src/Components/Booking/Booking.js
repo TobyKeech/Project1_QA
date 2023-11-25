cbb9f5dd795bef7543fac3a946e2c6c24bb7cc53
TobyKeech <toby.keech18@gmail.com>
2023-11-25 17:47:02 +0000

    buyers fetch on booking page working

--- src/Components/Booking/Booking.js
+++ src/Components/Booking/Booking.js
@@ -5,10 +5,6 @@ const Booking = () => {
 const [buyers, setBuyers] = useState([]);
 
 
-
-
-
-
 useEffect(()=>{
     fetch ("http://localhost:3000/buyer")
     //get the json content across
@@ -23,28 +19,24 @@ useEffect(()=>{
 
 .then(buyers => {
     setBuyers(buyers)
+    //sets the state of buyers above with the data from the response, done by using the setBuyers function into an empty array
     console.log(buyers)
     //check if the buyers object has been passed the information from JSON
 })
 .catch( error => console.error(error))
 //provides and error message if promise not caught and passed to the buyer fetch
 }, [])
+//stored in a lsit to access data later via the map function of JS
 
 
 
 
-
-
-
-
-
-
-    return ( 
+return ( 
 
         <div>
             this is the bookings page
         </div>
-     );
+        );
 }
  
 export default Booking;
--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -3,21 +3,28 @@ import React from "react";
 import "./LandingPage.css";
 
 function LandingPage() {
+//this is the home page of the application or the first page viewed upon loading
+
   return (
     <>
       <div className="container">
         <div className="header">Estate Agent</div>
           <nav className="row">
             <Link to="/property">&nbsp;Properties</Link>
+            {/* links to the property page via inetraction with the browser router */}
             <Link to="/seller">&nbsp;Seller</Link>
+             {/* links to the seller page via inetraction with the browser router */}
             <Link to="/buyer">&nbsp;Buyers</Link>
+             {/* links to the buyers page via interaction with the browser router */}
           </nav>
      
       </div>
 
       <div className="container">
         <Outlet />
+        {/* ^^show the default page upon rendering the web page for the frist time */}
       </div>
+      
     </>
   );
 }
