d142bdd66fa2a3084871083ca845ce058c27dd61
TobyKeech <toby.keech18@gmail.com>
2023-11-25 11:15:03 +0000

    adding fetch to properties page

--- src/Components/Property/Property.js
+++ src/Components/Property/Property.js
@@ -4,6 +4,33 @@ import { Link } from 'react-router-dom';
 
 
 const Property = () => {
+
+
+    const [loading, setLoading] = useState(true);
+    //state that can be changed by calling upon setLoading which will update the state in this case loading
+
+
+useEffect(()=> {
+        fetch("http://localhost:8081/property")
+        //get the json content from the backend server to render
+
+        .then((response)=> {
+            if( !response.ok) {
+                alert("Error occured, could not load data of properties")
+                throw response.status
+                //returns the status of the resonse back to the user ie 202 or 404
+            } else return response.json();
+            // if everything is good return the properties json data in the response
+        })
+
+        .then(properties) => {
+
+        }
+        
+})
+
+
+
     return ( 
         <>
         
