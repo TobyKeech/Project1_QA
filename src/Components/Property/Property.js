7a91dbaeaf7189c92a204767359b39eb5cf50ab5
TobyKeech <toby.keech18@gmail.com>
2023-11-25 11:48:33 +0000

    added property page data through fetch requst and stored in list

--- src/Components/Property/Property.js
+++ src/Components/Property/Property.js
@@ -23,11 +23,22 @@ useEffect(()=> {
             // if everything is good return the properties json data in the response
         })
 
-        .then(properties) => {
+        .then(properties => {
+                setLoading(false);
+                //manipulates the state to set this to false whena ction is complete, this started as true above in state 
+                console.log(properties)
+                //check what is contained within propeties, list of propeties displayed within the console
+        })
 
-        }
+        .catch(error => {
+            setLoading(false)
+            console.log(error)
+            alert("Error has occured getting the data")
+            //bring an alert if the json data doesnt load properly
+        })
         
-})
+},[])
+//this gets set into a new array to then use further down
 
 
 
@@ -35,7 +46,7 @@ useEffect(()=> {
         <>
         
         
-        <div>Property</div>
+        <div> this should Property</div>
         
         
         
