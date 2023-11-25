2a43149c9ea22d5d1259920b8f712984bac7421c
TobyKeech <toby.keech18@gmail.com>
2023-11-25 12:22:17 +0000

    fixed fetch and added state for false loading and error handling

--- src/Components/Buyer/Buyer.js
+++ src/Components/Buyer/Buyer.js
@@ -1,26 +1,41 @@
 import { useEffect, useState } from "react";
 
 const Buyer = () => {
-    const [loading, setLoading] = useState(false);
 
+    const [loading, setLoading] = useState(true);
+    //state that can be changed by calling upon setLoading which will update the state in this case loading to true or false
+    //by manipulating state
 
-    useEffect(() => {
-        setLoading(true);
-        fetch("http://localhost:8081/buyer")
-        //fetching content from backend
-        .then((response)=>{
-            if(!response.ok){
-                alert("Error has occured cannot load response. Unable to laod buyers")
+    useEffect(()=> {
+        setLoading(true)
+            fetch("http://localhost:8081/buyer")
+            //get the json content from the backend server to render
+    
+            .then((response)=> {
+                if( !response.ok) {
+                    alert("Error occured, could not load data of buyers")
+                    throw response.status
+                    //returns the status of the resonse back to the user ie 202 or 404
+                } else return response.json();
+                // if everything is good return the buyers json data in the response
+            })
+    
+            .then(buyers => {
+                    setLoading(false);
+                    //manipulates the state to set this to false whena ction is complete, this started as true above in state 
+                    console.log(buyers)
+                    //check what is contained within propeties, list of propeties displayed within the console
+            })
+    
+            .catch(error => {
                 setLoading(false)
-                throw response.status;
-                //show the http status of the response    
-            } else return response.json;
-        })
-
-        
-    })
-
-
+                console.log(error)
+                alert("Error has occured getting the data")
+                //bring an alert if the json data doesnt load properly
+            })
+            
+    },[])
+    //this gets set into a new array to then use further down
 
    
     return (  
