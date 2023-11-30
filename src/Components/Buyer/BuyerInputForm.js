b129a54faf020c6e0506dc3bf1bb0d1f862f365d
TobyKeech <toby.keech18@gmail.com>
2023-11-30 15:58:05 +0000

    added buyer add button but issue on displaying noted in commit

--- src/Components/Buyer/BuyerInputForm.js
+++ src/Components/Buyer/BuyerInputForm.js
@@ -6,45 +6,60 @@ const  buyerAddHandler = props.buyerAddHandler
 
 
 const refFirstName = useRef();
-const refSecondName = useRef();
+const refSurname = useRef();
 const refAddress = useRef();
 const refPostcode = useRef();
 const refPhone = useRef();
 
-
+const sumbitHandler = (event) => {
+    event.preventDefault(); 
+     //sumbit handler for when the submit button is pressed and used by the user. If statement is run to check if the inputted values match those of the and then will then proceed to add these to the db json 
+         
+     if ( refFirstName.current.value && refSurname.current.value && refAddress.current.value && refPostcode.current.value && refPhone.current.value){
+         buyerAddHandler(
+             {
+                 "firstName": refFirstName.current.value,
+                 //adds the first name given the value of the submit provided in the text box of the form, same goes for all the others below 
+                 "surname": refSurname.current.value,
+                 "address": refAddress.current.value,
+                 "postcode": refPostcode.current.value,
+                 "phone": refPhone.current.value
+             }
+         )
+         } 
+ 
+ }
 
 
     return ( 
         <>
         <div>
             <label>First Name</label>
-            <input type="text" id="sellerFirstName" ref={refFirstName}/>
+            <input type="text" id="buyerFirstName" ref={refFirstName}/>
         </div>
 
         <div>
             <label>Second Name</label>
-            <input type="text" id="sellerSurname" ref={refSecondName}></input>
-            {/* the ref is given the ref set by use ref above which is started as empty. */}
+            <input type="text" id="buyerSecondName" ref={refSurname}></input>
         </div>
 
         <div>
             <label>Address</label>
-            <input type="text" id="sellerAddress" ref={refAddress}></input>
+            <input type="text" id="buyerAddress" ref={refAddress}></input>
         </div>
 
         <div>
             <label>Postcode</label>
-            <input type="text" id="sellerPostcode" ref={refPostcode}></input>
+            <input type="text" id="u" ref={refPostcode}></input>
         </div>
 
         <div>
             <label>Phone Number</label>
-            <input type="text" id="sellerPhoneNumber" ref={refPhone}></input>
+            <input type="text" id="buyerPhoneNumber" ref={refPhone}></input>
         </div>
 
         <div>
-            <button type="submit" onClick={sumbitHandler}>Add New Seller</button>
-            {/* when the add new seller button is clicked the submitHandler function is called and the inputs are filled with the vlaues which were entered into the text inputs */}
+            <button type="submit" onClick={sumbitHandler}>Add New Buyer</button>
         </div>
         </>
      );
