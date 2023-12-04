483fee89d39ef2023d6e3ec7dbcdd47ffc10cb75
TobyKeech <toby.keech18@gmail.com>
2023-12-04 11:08:35 +0000

    Lyam fixed my navbar-teamwork makes the dreamwork

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,37 +3,41 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-sm navbar-light bg-dark">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link to="/property" className="navbar-brand text-white">
-              &nbsp;Trusted Tenancy's
-            </Link>
-            <Link to="/property" className="nav-item nav-link text-white">
-              &nbsp;Properties
-            </Link>
-            <Link to="/seller" className="nav-item nav-link  text-white">
-              &nbsp;Seller
-            </Link>
-            <Link to="/buyer" className="nav-item nav-link  text-white">
-              &nbsp;Buyers
-            </Link>
-          </div>
-        </div>
-      </nav>
-      <div className="mt-5">
+     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+  <button
+    className="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbarNavAltMarkup"
+    aria-controls="navbarNavAltMarkup"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav mr-auto">
+      <Link to="/property" className="navbar-brand text-white">
+        &nbsp;Trusted Tenancy's
+      </Link>
+    </div>
+    <div className="navbar-nav ms-auto">
+      
+      <Link to="/property" className="nav-item nav-link text-white">
+        &nbsp;Properties
+      </Link>
+      <Link to="/seller" className="nav-item nav-link  text-white">
+        &nbsp;Seller
+      </Link>
+      <Link to="/buyer" className="nav-item nav-link  text-white">
+        &nbsp;Buyers
+      </Link>
       </div>
+    </div>
+  
+</nav>
+<span className="mt-5"></span>
+
     </>
   );
 };
