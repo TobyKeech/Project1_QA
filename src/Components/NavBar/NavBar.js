bb21f5481a26d5d0080ecbfb78464ec6a6b60f30
TobyKeech <toby.keech18@gmail.com>
2023-12-04 11:18:21 +0000

    icons for navbar added

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faHouseUser, faHouseFlag } from "@fortawesome/free-solid-svg-icons"
 
 const NavBar = () => {
   return (
     <>
-     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-sm navbar-dark" style={{ backgroundColor: '#f1e0c6' }}>
   <button
     className="navbar-toggler"
     type="button"
@@ -24,19 +26,19 @@ const NavBar = () => {
     <div className="navbar-nav ms-auto">
       
       <Link to="/property" className="nav-item nav-link text-white">
-        &nbsp;Properties
+        &nbsp;Properties <FontAwesomeIcon icon={faHouse}/>
       </Link>
       <Link to="/seller" className="nav-item nav-link  text-white">
-        &nbsp;Seller
+        &nbsp;Seller <FontAwesomeIcon icon={faHouseFlag}/>
       </Link>
       <Link to="/buyer" className="nav-item nav-link  text-white">
-        &nbsp;Buyers
+        &nbsp;Buyers <FontAwesomeIcon icon={faHouseUser}/>
       </Link>
       </div>
     </div>
   
 </nav>
-<span className="mt-5"></span>
+<div className="mt-5"></div>
 
     </>
   );
