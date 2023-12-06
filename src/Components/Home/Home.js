f9d103100b839538036fca9fffc2d55355a0d205
TobyKeech <toby.keech18@gmail.com>
2023-12-06 20:36:52 +0000

    added home page image

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,32 +1,41 @@
+
 const Home = () => {
-  return (
-    <>
-     <div className="d-flex align-items-center justify-content-center" >
-        <h1 className="display-1 text-center">Rook Residencies</h1>
-    </div>
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src={require("../../images/img1.jpg")} alt="First slide"/>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src={require("../../images/img1.jpg")} alt="Second slide"/>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src={require("../../images/img1.jpg")} alt="Third slide"/>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-    </>
-  );
+    return (
+        <>
+        <div className="container">
+            <div className="d-flex align-items-center justify-content-center">
+                <h1 className="display-1 text-center">Rook Residencies</h1>
+            </div>
+            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100" src={require("../../images/chessimg.jpg")} alt="First slide" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={require("../../images/img2.jpg")} alt="Second slide" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={require("../../images/img3.jpg")} alt="Third slide" />
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
+            </div>
+            <div className="d-flex align-items-center justify-content-center">
+                <h1 className="display-1 text-center">Helping you make the right move.</h1>
+            </div>
+            <br />
+            <br />
+        </>
+    );
+ 
 };
 
 export default Home;
