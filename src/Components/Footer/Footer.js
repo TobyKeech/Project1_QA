30742ee875c6d335912b2af3cf15501a9071aab6
Toby Keech <Toby.Keech@embark.co.uk>
2024-02-07 09:46:01 +0000

    added titles to the footer social media links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,16 +10,16 @@ const Footer = () => {
   <div class="d-flex align-items-center">
     <span class="me-3">Connect with us on socials:</span>
 
-    <a href="" class="me-4 text-reset fa-lg">
+    <a href="" title="instagram logo" class="me-4 text-reset fa-lg">
       <FontAwesomeIcon icon={faInstagram} style={{ color: "#bb06e0" }} />
     </a>
-    <a href="" class="me-4 text-reset fa-lg">
+    <a href="" title = "twitter logo" class="me-4 text-reset fa-lg">
       <FontAwesomeIcon icon={faTwitter}  style={{ color: "#0792e9" }} />
     </a>
-    <a href="" class="me-4 text-reset fa-lg">
+    <a href=""  title = "facebook logo"class="me-4 text-reset fa-lg">
       <FontAwesomeIcon icon={faFacebook}  style={{ color: "#0822e2" }} />
     </a>
-    <a href="" class="me-4 text-reset fa-lg">
+    <a href=""  title = "linkedin logo " class="me-4 text-reset fa-lg">
       <FontAwesomeIcon icon={faLinkedin}  style={{ color: "#0792e9" }} />
     </a>
   </div>
